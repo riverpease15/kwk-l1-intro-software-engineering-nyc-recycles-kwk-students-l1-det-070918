2a4a7d1d75317a128205aad1cc32c0675e28e09c
River Pease <riverpease15@gmail.com>
2018-07-12 18:09:52 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -35,7 +35,9 @@ end
 #You should use the `each_key` method to iterate over your hash and print out each key.
 
 def all_keys(first_recycle_bin)
-  return first_recycle_bin["all keys"]
+  first_recycle_bin.each do |keys, values|
+    puts "#{keys}"
+  end
 end
 
 #4. Define a method called `all_data` that takes a hash as an argument.
@@ -43,9 +45,16 @@ end
 #You should use the `each_value` method to iterate over your hash and print out each value.
 
 def all_data(first_recycle_bin)
-  return first_recycle_bin["address"]
+  first_recycle_bin.each do |keys, values|
+    puts "#{values}"
+  end
 end
 
 #5. Define a method called `key_and_data` that takes a hash as an argument.
 #This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
 #You should use the `each` method to iterate over your hash to print out that sentence
+
+def key_and_data(first_recycle_bin)
+  first_recycle_bin.each do |keys, values|
+    puts "The #{keys} is #{values}""
+  
